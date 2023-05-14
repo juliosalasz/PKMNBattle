e176971379102c2e2c599e385ec0ee1d1bcf82b2
Julio Salas <jscomic@gmail.com>
2023-05-14 17:13:26 -0500

    conditional to winner

--- src/app/winner/page.jsx
+++ src/app/winner/page.jsx
@@ -39,7 +39,7 @@ export default function Winner() {
         </div>
         <div>
           <h2>Here is your winner</h2>
-          <PokemonCard pkmnArray={selectedPkmn} />
+          {pkmnArray ? <PokemonCard pkmnArray={selectedPkmn} /> : ""}
           <button onClick={goback}>Start Again</button>
         </div>
       </div>
