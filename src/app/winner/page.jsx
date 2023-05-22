dab1bd5db2a7cdc22fa085c8b6b7cb19a2674064
Julio Salas <jscomic@gmail.com>
2023-05-21 22:56:17 -0500

    winner page progress. Missing footer

--- src/app/winner/page.jsx
+++ src/app/winner/page.jsx
@@ -1,9 +1,12 @@
 "use client";
 import React from "react";
 import PokemonCard from "../../../components/pokemonCard/pokemonCard";
-import Image from "next/image";
 import { useGlobalContext } from "../../../context/pkmnContext";
 import { useRouter } from "next/navigation";
+import styles from "./winner.module.css";
+
+/* For Later use
+<footer className={styles.footerPkmn}>{selectedPkmn.name}</footer> */
 
 export default function Winner() {
   const { selectedPkmn, setPkmnArray } = useGlobalContext();
@@ -13,12 +16,23 @@ export default function Winner() {
     setPkmnArray();
   };
   return (
-    <main>
-      <div>
-        <h2>Here is your winner</h2>
-        {selectedPkmn ? <PokemonCard pkmnArray={selectedPkmn} /> : ""}
-        <button onClick={goback}>Start Again</button>
-      </div>
-    </main>
+    <>
+      <main className={styles.mainWinner}>
+        <div className={styles.winnerText}>
+          <h2>
+            HERE IS <br></br>YOUR
+            <span className={styles.winnerWhite}> WINNER!</span>
+          </h2>
+        </div>
+        <div>
+          {selectedPkmn ? <PokemonCard pkmnArray={selectedPkmn} /> : ""}
+        </div>
+        <div className={styles.startAgain}>
+          <button className={styles.startAgainBtn} onClick={goback}>
+            TRY<br></br>AGAIN?
+          </button>
+        </div>
+      </main>
+    </>
   );
 }
